09fb3c1716cfa2f47cbbb031a24907ffd456b949
bjorgeh <19abh92@gmail.com>
2023-11-03 15:07:23 +0100

    opprettet flere klasser
    organisert kode bedre og gjør klar til session implementering

--- API.py
+++ API.py
@@ -1,112 +1,107 @@
 #imports flask & flask_restx for creating API
-from flask import Flask, request
+from flask import Flask, request, url_for, jsonify
 from flask_restx import Api, Resource, fields
+from flask_session import Session
+#import datetime
+import datetime
 #imports os for getting current working directory
 import os
 #Imports function for requiring API key
 from functools import wraps
-from flask import jsonify
 #imports authorization file
 from authorization import apiKeyAuth as auth
+from authorization import session_config as SessionCFG
+
 #imports fnction for creating new user and database
-from SQLAdminConnections import SQL_CreateNewLeaderUser
-from SQLAdminConnections import SQL_CreateNewOperatorUser
+#from SQLAdminConnections import SQL_CreateNewLeaderUser
+#from SQLAdminConnections import SQL_CreateNewOperatorUser
+
 #imports function for requiring API key
 from authorization import api_key as key
 #imports user models
 from Models.user_model import *
+#PasswordHandler
 from PW_hashHandler import pw_manager as hash
+#imports user object
+from USER_obj import users as USR
+from USER_obj import new_user as makeUSR
 
 #print(os.getcwd()) #uncomment for troubleshooting to see current working directory
 
-#Checks if API key is correct, if not returns error message
-def require_api_key(func):
-    @wraps(func)
-    def decorated(*args, **kwargs):
-        if request.headers.get('x-api-key') == key.getAPIKey():
-            return func(*args, **kwargs)
-        else:
-            return {"error": "API key missing or invalid"}, 401  # 401 = Unauthorized
-    return decorated
-
-#Gets api key settings from authorization file
-ApiKey_settings = auth.API_KEY_SETTINGS
-
 #defines app and api
 app = Flask(__name__)
+
+app.config.from_object(SessionCFG)
 
 #defines api
 api = Api(app,
           version='1.0',
           title='RapportSystem API Doc',
           description='Overview of the API endpoints',
-          doc='/api/',
-          authorizations=ApiKey_settings)
+          doc='/api/'
+        )
 
-#gets user models
-new_leader_user_model = leader_user_model(api)
-new_operator_user_model = operator_user_model(api)
+#gets and implements user models
+new_user_model = user_model(api)
+new_login_model = login_model(api)
 
 #defines namespace
-ns = api.namespace('endpoints', description='API Endpoints')
+ns = api.namespace('api', description='API Endpoints')
+
+#Post request for creating a new leader user & belonging database
+@ns.route('/login')
+class login(Resource):
+    @api.doc('login')
+
+    #expects user model from post request
+    @api.expect(new_login_model, validate=True)
+    def post(self):
+
+        #Gets data from post request
+        data = request.get_json()
+
+        #Gets username and password from data
+        username = data["username"]
+        password = data["password"]
+
+        '''
+        Her skal sjekkes om bruker eksisterer i databasen og om passord er riktig.
+        Hvis bruker eksisterer og passord er korrekt, skal det opprettes et user objekt - Her kan man også sjekke session osv.
+        '''
+
+        #Change print with password check
+        print(username, password)
+
+        #returns error if no data is found or faulty
+        if not data:
+            return {"Error": "No data"}, 400
+        return data
 
 #Get request for testing API connection
 @ns.route('/test')
 class Test(Resource):
     @api.doc('get_test')
 
-    #requires API key to be used
-    @api.doc(security='apiKey')
-    #requires API key
-    @require_api_key
-
     def get(self):
-        '''Fetch a test data'''
+        #returns test data
         return {"Test": "OK"}
 
 #Post request for creating a new leader user & belonging database
-@ns.route('/createLeaderUser')
+@ns.route('/createUser')
 class CreateLeaderUser(Resource):
-    @api.doc('create_leader_user')
-
-    #requires API key to be used
-    @api.doc(security='apiKey')
-    #requires API key
-    @require_api_key
+    @api.doc('create_user')
 
     #expects user model from post request
-    @api.expect(new_leader_user_model, validate=True)
+    @api.expect(new_user_model, validate=True)
     def post(self):
 
         #Gets data from post request
         data = request.get_json()
 
-        #Creates new user and database from data
-        SQL_CreateNewLeaderUser.createNewLeaderUser(data["email"], hash.hash(data['userPass']), data["databaseName"])
-
-        #returns error if no data is found or faulty
-        if not data:
-            return {"Error": "No data"}, 400
-        return data
-    
-#Post request for creating a new operator user
-@ns.route('/createOperatorUser')
-class CreateUser(Resource):
-    @api.doc('create_leader_user')
-
-    #requires API key to be used
-    @api.doc(security='apiKey')
-    #requires API key
-    @require_api_key
-
-    #expects user model from post request
-    @api.expect(new_operator_user_model, validate=True)
-    def post(self):
-
-        #Gets data from post request
-        data = request.get_json()
-        #Creates new user from data
-        SQL_CreateNewOperatorUser.createNewOperatorUser(data["email"], hash.hash(data['userPass']))
+        #Makes new User Objekt
+        new_user = USR.users(data["email"], hash.hash(data['password']), data["accountType"])
+        #Creates new user in database
+        makeUSR.createUser(new_user).saveToDB()
 
         #returns error if no data is found or faulty
         if not data:
---
+++ USER_obj/users.py
@@ -0,0 +1,111 @@
+import os
+#Gets current directory
+current_directory = os.getcwd()
+#imports sys
+import sys
+sys.path.append(os.path.join(current_directory))
+
+#Imports SQLAdminConnections modules
+from SQLAdminConnections import SQL_AdminConnector as SQLC
+from SQLAdminConnections import SQL_AdminQuerys as SQLQ
+
+#user class for handeling user data
+class users:
+    def __init__(self,email,password,accountType,databaseName=None):
+        self.email = email
+        self.password = password
+        self.databaseName = databaseName
+        self.accountType = accountType
+        self.sessionID = None
+
+    #updates user email
+    def updateEmail(self,email):
+        if not email:
+            return 'Email not updated'
+
+        self.email = email
+        return 'new email:' + email
+    
+    #updates user password
+    def updatePassword(self,password):
+        if not password:
+            return 'Password not updated'
+
+        self.password = password
+        return 'Password updated!'
+    
+    #updates user database name
+    def updateDatabaseName(self,databaseName):
+        if not databaseName:
+            return 'Database name not updated'
+        self.databaseName = databaseName
+        return 'Database name updated:' + databaseName
+    
+    #updates user account type
+    def updateAccountType(self,accountType):
+        if not accountType:
+            return 'Account type not updated'
+        self.accountType = accountType
+        return 'Account type updated:' + accountType
+    
+    #returns user email
+    def getEmail(self):
+        if not self.email:
+            return 'No email found'
+        return self.email
+    
+    #returns user password
+    def getPassword(self):
+        if not self.password:
+            return 'No password found'
+        return self.password
+    
+    #returns user database name
+    def getDatabaseName(self):
+        if not self.databaseName:
+            return 'No database name found'
+        return self.databaseName
+    
+    #returns user account type
+    def getAccountType(self):
+        if not self.accountType:
+            return 'No account type found'
+        return self.accountType
+    
+    #returns user session ID
+    def getAccountType(self):
+        if not self.sessionID:
+            return 'No sessionID type found'
+        return self.sessionID
+    
+    #Returns true if leader, else false
+    def isLeader(self):
+        return self.accountType == 'leader'
+    #returns true if operator, else false
+    def isOperator(self):
+        return self.accountType == 'operator'
+    #returns true if admin, else false
+    def isAdmin(self):
+        return self.accountType == 'admin'
+
+    #returns true if session expired, else false
+    def isSessionExpired(self):
+        return self.internalSC(self.sessionID)    
+
+    #retuns true if session expired, else false
+    def internalSC(session_id):
+
+        #connect to database
+        connection = SQLC.SQLConAdmin()
+
+        #Execute query
+        connection.execute_query(SQLQ.SQLQueries.check_session_expired(session_id))
+        
+        #get result
+        (count,) = connection.cursor.fetchone()
+
+        #close connection
+        connection.close()
+
+        #returns true if session is expired, else false
+        return count > 0
