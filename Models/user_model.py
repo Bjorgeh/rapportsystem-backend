0b89290573b28778b0ea2b58d53c89c5a2967166
bjorgeh <bjorgen92@gmail.com>
2023-11-14 01:22:55 +0100

    Models_fix

--- Models/user_model.py
+++ Models/user_model.py
@@ -1,5 +1,6 @@
 from flask_restx import fields
-''''
+
+"""'
 #defines leader model
 def leader_user_model(api):
     return api.model('LeaderUser', {  
@@ -14,26 +15,55 @@ def operator_user_model(api):
         'email': fields.String(required=True, description='User email'),
         'userPass': fields.String(required=True, description='User password')
     })
-'''
+"""
 
-#defines login model
+
+# defines login model
 def login_model(api):
-    return api.model('Login', { 
-        'username': fields.String(required=True, description='User email'),
-        'password': fields.String(required=True, description='User password')
-    })
+    return api.model(
+        "Login",
+        {
+            "username": fields.String(
+                required=True, description="User email", example="ola.nordmann@viken.no"
+            ),
+            "password": fields.String(
+                required=True, description="User password", example="EpicPassword69"
+            ),
+        },
+    )
 
-#defines login model
+# defines login model
 def user_model(api):
-    return api.model('New user', { 
-        'email': fields.String(required=True, description='User email'),
-        'password': fields.String(required=True, description='User password'),
-        'accountType': fields.String(required=True, description='Account type | admin/leader/operator')
-    })
+    return api.model(
+        "New user",
+        {
+            "email": fields.String(
+                required=True, description="User email", example="ola.nordmann@viken.no"
+            ),
+            "password": fields.String(
+                required=True, description="User password", example="EpicPassword69"
+            ),
+            "accountType": fields.String(
+                required=True,
+                description="Account type | admin/leader/operator",
+                example="operator",
+            ),
+        },
+    )
 
-#defines login model
+
+# defines login model
 def update_password_model(api):
-    return api.model('New password', { 
-        'password1': fields.String(required=True, description='New Password'),
-        'password2': fields.String(required=True, description='Confirmed Password')
-    })
+    return api.model(
+        "New password",
+        {
+            "password1": fields.String(
+                required=True, description="New Password", example="EpicPassword69"
+            ),
+            "password2": fields.String(
+                required=True,
+                description="Confirmed Password",
+                example="EpicPassword69",
+            ),
+        },
+    )
